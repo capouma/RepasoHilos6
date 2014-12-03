b6db72f1d6a2cafdf9f542fc410e317b22072ca3
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 20:00:46 +0100

    Iniciamos el ejercicio de repaso 6.

---
+++ repaso6/Repaso6.java
@@ -0,0 +1,22 @@
+/*
+ * Realiza un programa en java que ejecute tres hilos de forma concurrente.
+ * Uno de ellos debe sumar los números pares del 1 al 1000, otro los impares,
+ * y otro, los que terminan en dos o en tres.
+ */
+package repaso6;
+
+/**
+ *
+ * @author findag
+ */
+public class Repaso6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
