eb5d25a39e3e4ee0f2b38d97e89cbfb8dfcf8983
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-12-03 20:29:33 +0100

    Creamos un switch en el run para controlar que hilo esta actuando, de manera que dependiendo que hilo se este ejecutando sume una cosa u otra.

--- repaso6/Hilo.java
+++ repaso6/Hilo.java
@@ -9,6 +9,53 @@ package repaso6;
  *
  * @author findag
  */
-public class Hilo {
+public class Hilo extends Thread
+{
+    int contador;
+    String texto;
+    public Hilo(String nombre)
+    {
+        super(nombre);
+    }
     
+    public void run()
+    {
+        // Creamos un switch que dependiendo del nombre del hilo entrara en un caso u en otro, mostrando el resultado al final.
+        switch(getName())
+        {
+            case "Hilo 1":
+                for(int i = 0; i <= 10; i ++)
+                {
+                    if(i%2 == 0)
+                    {
+                        contador = contador + i;
+                        
+                    }
+                }
+                texto = "pares";
+            break;
+            case "Hilo 2":
+                for(int i = 0; i <= 1000; i ++)
+                {
+                    if(i%2 != 0)
+                    {
+                        contador = contador + i;
+                    }
+                }
+                texto = "impares";
+            break;
+            case "Hilo 3":
+                for(int i = 0; i <= 10; i ++)
+                {
+                    if(String.valueOf(i).endsWith("2") || String.valueOf(i).endsWith("3"))
+                    {
+                        contador = contador + i;
+                    }
+                }
+                texto = " terminados en 2 y en 3";
+            break;
+        }
+        System.out.println("El total de numeros " + texto + " suma: " + contador);
+        
+    }
 }
